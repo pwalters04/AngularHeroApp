e232dea7b89e4469cdfd7bcc85f6a04c1eebfb35
Paris N Walters <pwalters2004@gmail.com>
2017-05-21 16:57:27 -0400

    src files

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import{ routing } from './app.routing';
+import {HeroService} from './hero.service';
+
+import { AppComponent } from './app.component';
+import {HeroDetailComponent} from './hero-detail.component';
+import {HeroesComponent} from './heroes.component';
+import {DashboardComponent} from './dashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    routing,
+    HttpModule
+  ],
+  providers: [HeroService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/mock-heros.ts
@@ -0,0 +1,15 @@
+import {Hero} from './hero';
+
+export const HEROES: Hero[] = [
+  {id:11, name:'Paris Walters'},
+  { id:12, name:'Captain Planet'},
+  { id:13, name:'Captain Obvious'},
+  { id:14, name: 'Batman'},
+  { id:15, name:'Superman'},
+  { id:16, name:'Batwoman'},
+  { id:17, name:'Catwoman'},
+  { id:18, name:'Magneto'},
+  { id:19, name:'Nightcrawler'},
+  { id:20, name:'Beast'},
+  { id:21 , name:'Captain America'},
+];
